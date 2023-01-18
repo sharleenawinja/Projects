4e0eb02f701f518959c9823f04693f3605236903
SharleenAwinja <awinjasharleen@gmail.com>
2023-01-18 10:44:15 +0300

    twoSum

--- twoSum.js
+++ twoSum.js
@@ -27,3 +27,23 @@ function twoSum(nums, target) {
 console.log(twoSum([2, 7, 11, 5], 9)); //expected [0, 1]
 console.log(twoSum([3, 2, 4], 6));  //expected [1, 2]
 console.log(twoSum([15, 7, 11, 2], 9)); //expected [1, 3]
+
+//Alternative approach
+//the variable j should not be indexed from 0 because it will cause whatever element that resides at i to be considered twice
+//j should always be i + 1
+// for example given the array [3, 2, 4] and the target is 6; if both i and j are initialized as 0, then 3 + 3 will give six and the output will be [0, 0]; the problem with this is that no element in the array should be used twice
+const sumOfTwo = (numbers, result) => {
+    let length = numbers.length;
+    for (let i = 0; i < length; i++) {
+        for(let j = i + 1; j < length; j++) {
+            let total = numbers[i] + numbers[j];
+            if(total === result) {
+                return [i, j];
+            }
+        }
+    }
+};
+
+console.log(sumOfTwo([2, 7, 11, 5], 9)); //expected [0, 1]
+console.log(sumOfTwo([3, 2, 4], 6));  //expected [1, 2]
+console.log(sumOfTwo([15, 7, 11, 2], 9)); //expected [1, 3]
