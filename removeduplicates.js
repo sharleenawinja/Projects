fc2dedd8b5debb2c4c69e358a4af79ab1347261a
SharleenAwinja <awinjasharleen@gmail.com>
2023-01-26 14:22:25 +0300

    remove duplicates

--- removeduplicates.js
+++ removeduplicates.js
@@ -1,5 +1,5 @@
 //Given a sorted array nums, remove the duplicates in-place 
-//such that each element appear only once and return the new length.
+//such that each element appears only once and return the new length.
 
 function removeDuplicates(arr) {
     const arrlength = arr.length;
