1a3bd470109181e27740187414dfeb794d6aebd6
SharleenAwinja <awinjasharleen@gamil.com>
2023-02-03 11:56:28 +0300

    java lessons

--- Java/Hello.java
+++ Java/Hello.java
@@ -12,5 +12,16 @@ public class Hello {
         //The println() method, short for "print line", is used to print a value to the screen or a file.
         //Each code statement should end in a semicolom (;)
         System.out.println("Hello World!");
+
+        //There is also a print() method but it does not insert a new line
+        //Println() makes it easier to read the output of the code
+        System.out.print("Learning Java");
+        System.out.println("Starting today");
+
+        //When printing numbers dont use double quotes
+        System.out.println(20);
+
+        //You can also perfom mathematical calculations
+        System.out.println(5+4);
     }
 }
