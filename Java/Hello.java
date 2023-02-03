1b317184913a4649cea7be21dfbebd03e6b5c934
SharleenAwinja <awinjasharleen@gamil.com>
2023-02-03 10:19:34 +0300

    Starting java

---
+++ Java/Hello.java
@@ -0,0 +1,16 @@
+//Every line of code in Java must be in a class
+// Java is case sensitive "MyClass" and "myclass" have different meanings
+//The name of the java file must match the class name
+
+public class Hello {
+
+//Every project must contain the main() method
+//Any code inside main will be executed
+    public static void main(String[] args) {
+
+        //System is a built in java class that contains useful members, such as out which is short for output
+        //The println() method, short for "print line", is used to print a value to the screen or a file.
+        //Each code statement should end in a semicolom (;)
+        System.out.println("Hello World!");
+    }
+}
