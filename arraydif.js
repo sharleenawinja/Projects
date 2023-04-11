c99d8584d8e9a17d5a1ad542bb846a15131105d2
sharleenawinja <awinjasharleen@gmail.com>
2023-04-11 16:56:01 +0300

    merge sorted array

--- arraydif.js
+++ arraydif.js
@@ -150,3 +150,37 @@ function averageScore(data) {
 }
 
 console.log(averageScore(data)); // 82.5
+
+// merge two sorted arrays into one sorted array
+const arra = [1, 3, 5, 7];
+const arrb = [2, 4, 6, 8];
+
+function mergeSortedArrays(arr1, arr2) {
+  const merged = [];
+  let i = 0;
+  let j = 0;
+
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] < arr2[j]) {
+      merged.push(arr1[i]);
+      i++;
+    } else {
+      merged.push(arr2[j]);
+      j++;
+    }
+  }
+
+  while (i < arr1.length) {
+    merged.push(arr1[i]);
+    i++;
+  }
+
+  while (j < arr2.length) {
+    merged.push(arr2[j]);
+    j++;
+  }
+
+  return merged;
+}
+
+console.log(mergeSortedArrays(arra, arrb)); // [1, 2, 3, 4, 5, 6, 7, 8]
