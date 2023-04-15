4af692146a45712bae7721251a3d723d6cc8f226
sharleenawinja <awinjasharleen@gmail.com>
2023-04-15 17:45:09 +0300

    create string variable

---
+++ Java/Strrings.java
@@ -0,0 +1,12 @@
+public class Strrings {
+    public static void main(String[] args) {
+        // Strings are used for storing text.
+        // A String variable contains a collection of characters surrounded by double quotes:
+
+        // Create a variable of type String and assign it a value:
+        String greeting = "Hello";
+
+        // print
+        System.out.println(greeting);
+    }
+}
